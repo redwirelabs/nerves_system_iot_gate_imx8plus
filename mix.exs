da42713edd91bdf9e9284da540789d12d3167998
Alex McLain <alex@alexmclain.com>
2025-09-13 12:31:10 -0700

    Update docs assets for new hex version

--- mix.exs
+++ mix.exs
@@ -77,7 +77,7 @@ defmodule NervesSystemIotGateIMX8Plus.MixProject do
     [
       extras: ["README.md", "CHANGELOG.md"],
       main: "readme",
-      assets: "assets",
+      assets: %{"assets" => "assets"},
       source_ref: "v#{@version}",
       source_url: @source_url,
       skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
